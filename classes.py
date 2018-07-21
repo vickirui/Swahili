aad4cd24b53a6997e2d851107fe2f85c8b0da967
Vickirui <victorkiprono472@gmail.com>
2018-07-21 11:59:25 +0300

    Another short code

---
+++ classes.py
@@ -0,0 +1,10 @@
+class Employee():
+     def __init__(self,name='',regNo='',gender=''):   
+        self.name=name
+        self.regNo=regNo
+        self.gender=gender
+		
+	
+emp5.name=input("Enter name: ")
+emp5.gender=input("Enter gender: ")
+emp5.regNo=input("Enter regNo: ")
